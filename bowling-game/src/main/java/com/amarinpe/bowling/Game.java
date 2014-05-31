10425194608cc85d9b3801f215260730f30eb692
quiram <abraham.marin.perez@gmail.com>
2014-05-31 12:46:43 +0100

    third test added, calculating scores of spares

--- bowling-game/src/main/java/com/amarinpe/bowling/Game.java
+++ bowling-game/src/main/java/com/amarinpe/bowling/Game.java
@@ -2,15 +2,34 @@ package com.amarinpe.bowling;
 
 public class Game
 {
-	private int score = 0;
-	
+	private int[] rolls = new int[21];
+	private int currentRoll = 0;
+
 	void roll(int pins)
 	{
-		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 
 	int score()
 	{
+		int score = 0;
+		int frameIndex = 0;
+
+		for (int frame = 0; frame < 10; frame++)
+		{
+			if (isSpare(frameIndex)) // spare
+				score += 10 + rolls[frameIndex + 2];
+			else
+				score += rolls[frameIndex] + rolls[frameIndex + 1];
+
+			frameIndex += 2;
+		}
+
 		return score;
+	}
+
+	private boolean isSpare(int frameIndex)
+	{
+		return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
 	}
 }
--- bowling-game/src/test/java/com/amarinpe/bowling/GameTest.java
+++ bowling-game/src/test/java/com/amarinpe/bowling/GameTest.java
@@ -8,13 +8,13 @@ import static org.junit.Assert.assertEquals;
 public class GameTest
 {
 	private Game g;
-	
+
 	@Before
 	public void setup()
 	{
 		g = new Game();
 	}
-	
+
 	@Test
 	public void testGutterGame()
 	{
@@ -31,10 +31,36 @@ public class GameTest
 		assertEquals(20, g.score());
 	}
 
+	@Test
+	public void testOneSpare()
+	{
+		rollSpare();
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
+
+	@Test
+	public void testTenPinsInTwoRollsButNotSpare()
+	{
+		g.roll(0);
+		g.roll(5);
+		g.roll(5);
+		g.roll(2);
+		rollMany(16, 0);
+		assertEquals(12, g.score());
+	}
+
 	private void rollMany(int n, int pins)
 	{
 		for (int i = 0; i < n; i++)
 			g.roll(pins);
+	}
+
+	private void rollSpare()
+	{
+		g.roll(5);
+		g.roll(5);
 	}
 
 }
